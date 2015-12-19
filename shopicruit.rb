ca424152bc28d91fb3e9e412f08743b23b7eccc1
Abdullah <abd00s@users.noreply.github.com>
2015-12-19 14:33:29 -0800

    Translate desired variants to ruby objects

--- shopicruit.rb
+++ shopicruit.rb
@@ -1,9 +1,17 @@
 require_relative "Api"
 require_relative "Product"
+require_relative "Variant"
 
 class Shopicruit
   all_products = Api::get_products
   desirable_products = all_products.select { |product| ["Computer", "Keyboard"]
     .include?(product["product_type"]) }
-  desirable_products.map! { |product| Product.new(product)}
+  desirable_products.map! { |product| Product.new(product) }
+  desirable_variants = Array.new
+  desirable_products.each do |product|
+    product.variants.each do |variant|
+      desirable_variants << Variant.new(variant)
+    end
+  end
+  puts desirable_variants.count
 end
