d1106bcac10d067cd5f292edddc39040699a9c59
Abdullah <abd00s@users.noreply.github.com>
2015-12-19 12:59:09 -0800

    Remove redundant include

--- api.rb
+++ api.rb
@@ -1,8 +1,6 @@
 require "HTTParty"
 
 module Api
-  include HTTParty
-
   def self.get_products
     response = HTTParty.get('http://shopicruit.myshopify.com/products.json')
     response["products"]
