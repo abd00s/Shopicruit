fc5ccc713eea9799dbc720e5edc48c9cc744368f
Abdullah <abd00s@users.noreply.github.com>
2015-12-19 14:00:07 -0800

    Translate desired prodcuts to ruby objects

--- shopicruit.rb
+++ shopicruit.rb
@@ -1,9 +1,9 @@
-require "./Api"
+require_relative "Api"
+require_relative "Product"
 
 class Shopicruit
   all_products = Api::get_products
   desirable_products = all_products.select { |product| ["Computer", "Keyboard"]
     .include?(product["product_type"]) }
-  puts all_products.count
-  puts desirable_products.count
+  desirable_products.map! { |product| Product.new(product)}
 end
