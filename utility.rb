f701086b586d686cfb1ae467f91129e874e2df1c
Abdullah <abd00s@users.noreply.github.com>
2015-12-21 20:39:13 -0800

    Routing correctly based on limit; ADD COMMENTS AND METHOD NAME

--- utility.rb
+++ utility.rb
@@ -7,14 +7,11 @@ module Utility
 
   def method_name(variants, limit)
     if sum_of_all_desired_variants(variants) <= limit
-      puts "all"
       # buy all
     elsif find_combinations(variants, limit).empty?
-      puts "none"
       # buy none
     else
       # COMBS << find_combinations(variants, limit)
-      print find_combinations(variants, limit)
     end
   end
 
@@ -23,14 +20,9 @@ module Utility
     variants.size.downto(1) do |i|
       unless variants.combination(i).select {|a| a.reduce(:+) <= limit}.empty?
         return variants.combination(i).select {|a| a.reduce(:+) <= limit}
-        # break
       end
-      # return []
-      # if none, return value is size of range // fix this
     end
+    return []
   end
-
-  x=[2,4,6,8,10,22]
-  method_name(x,10)
 end
 
