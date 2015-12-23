c5674d78b91583f794e1e80dd4ef92c2fa6d5d3a
Abdullah <abd00s@users.noreply.github.com>
2015-12-23 13:54:23 -0800

    Rename method for clarity and general use

--- utility.rb
+++ utility.rb
@@ -1,12 +1,12 @@
 module Utility
   extend self
 
-  def sum_of_all_desired_variants(arr)
+  def weight_of_variants(arr)
     arr.inject(1){ |sum, variant| sum + variant.grams }
   end
 
   def method_name(variants, limit)
-    if sum_of_all_desired_variants(variants) <= limit
+    if weight_of_variants(variants) <= limit
       # buy all
     elsif find_combinations(variants, limit).empty?
       # buy none
