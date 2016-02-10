1a33e00128e0da6cdd1adc81aa850d336dadca52
Abdullah <abd00s@users.noreply.github.com>
2016-02-10 14:42:02 -0500

    Move util module into program

--- shopicruit.rb
+++ shopicruit.rb
@@ -4,11 +4,10 @@ require_relative "Variant"
 require_relative "Utility"
 
 class Shopicruit
-  attr_reader :desirable_variants, :limit
-  extend Utility
+  attr_reader :desirable_variants, :limit, :products_to_purchase
 
   def initialize
-    @limit = 1000
+    @limit = 1000000
     @desired_categories = ["Computer", "Keyboard"]
     @desirable_products = []
     @desirable_variants = []
@@ -31,11 +30,45 @@ class Shopicruit
     end
   end
 
+  def weight_of_variants(arr)
+    arr.inject(1){ |sum, variant| sum + variant.grams }
+  end
+
+  def find_carriable_combo(variants, limit)
+    if weight_of_variants(variants) <= limit
+      # buy all
+      @products_to_purchase << "SSSSS"
+      message = "The total weight of all desired variants is under the weight limit
+      you may purchase all."
+    elsif find_combinations(variants, limit).empty?
+      # buy none
+      message = "All variants are too heavy, you can't purchase any."
+    else
+      # COMBS << find_combinations(variants, limit)
+      message = "This selection of variants is the most you can carry while
+      remaining under the limit"
+      # if there are 2 combs, select the cheaper one
+    end
+  end
+
+  def find_combinations(variants, limit)
+    # Decrementing iterator; Start at combination size == number of possible variants
+    variants.size.downto(1) do |i|
+      # Is there such a combination at this size? (i.e. satisfies weight limit)
+      unless variants.combination(i).select {|comb| weight_of_variants(comb) <= limit}.empty?
+        return variants.combination(i).select {|comb| weight_of_variants(comb) <= limit}
+      end
+    end
+    # Return an empty array if the limit cannot be satisfied
+    # i.e. lightest variant is heavier than the limit
+    return []
+  end
+
   def self.run
     shopicruit = Shopicruit.new
     shopicruit.filter_products
     shopicruit.find_all_desirable_variants
-    find_carriable_combo(shopicruit.desirable_variants, shopicruit.limit)
+    shopicruit.find_carriable_combo(shopicruit.desirable_variants, shopicruit.limit)
   end
 
 end
