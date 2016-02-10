e5364a38daade3265f50f7b7cacbd7c5b4ab9976
Abdullah <abd00s@users.noreply.github.com>
2016-02-10 14:03:14 -0500

    Mehtodise product filtration by cat

--- shopicruit.rb
+++ shopicruit.rb
@@ -6,7 +6,7 @@ require_relative "Utility"
 class Shopicruit
   extend Utility
 
-  def initialise
+  def initialize
     @limit = 1000
     @desired_categories = ["Computer", "Keyboard"]
     @desirable_products = []
@@ -15,14 +15,20 @@ class Shopicruit
     @message = []
   end
 
-  all_products = Api::get_products
-  desirable_products = all_products.select { |product| ["Computer", "Keyboard"]
-    .include?(product["product_type"]) }
-  desirable_products.map! { |product| Product.new(product) }
-  desirable_variants = Array.new
-  desirable_products.each do |product|
-    product.variants.each do |variant|
-      desirable_variants << Variant.new(variant)
+  def filter_products
+    all_products = Api::get_products
+    @desirable_products = all_products.select { |product| @desired_categories
+      .include?(product["product_type"]) }
+    @desirable_products.map! { |product| Product.new(product) }
+  end
+
+  def find_all_desirable_variants
+    @desirable_products.each do |product|
+      product.variants.each do |variant|
+        @desirable_variants << Variant.new(variant)
+      end
     end
   end
+
 end
+
