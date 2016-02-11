ba0aeafaaa1f2db82534b9f1c947445d656624cc
Abdullah <abd00s@users.noreply.github.com>
2016-02-11 14:02:02 -0500

    Move execution to sep file

---
+++ app.rb
@@ -0,0 +1,19 @@
+require_relative "Shopicruit"
+
+class App
+  def self.run
+    shopicruit = Shopicruit.new
+    shopicruit.filter_products
+    shopicruit.find_all_desirable_variants
+    shopicruit.find_carriable_combo(shopicruit.desirable_variants, shopicruit.limit)
+    puts shopicruit.message
+    shopicruit.products_to_purchase.each do |product|
+      puts "\t#{product.parent.title}"
+      puts "\t\t#{product.title}"
+    end
+    puts "Weighing #{(shopicruit.weight_of_variants(shopicruit.products_to_purchase)).to_f/1000} KGs"
+    puts "Costing you $#{shopicruit.price_of_variants(shopicruit.products_to_purchase)}"
+  end
+end
+
+App.run
--- shopicruit.rb
+++ shopicruit.rb
@@ -1,7 +1,6 @@
 require_relative "Api"
 require_relative "Product"
 require_relative "Variant"
-require_relative "Utility"
 
 class Shopicruit
   attr_reader :desirable_variants, :limit, :products_to_purchase, :message, :desirable_products
@@ -84,20 +83,4 @@ class Shopicruit
     return []
   end
 
-  def self.run
-    shopicruit = Shopicruit.new
-    shopicruit.filter_products
-    shopicruit.find_all_desirable_variants
-    shopicruit.find_carriable_combo(shopicruit.desirable_variants, shopicruit.limit)
-    puts shopicruit.message
-    shopicruit.products_to_purchase.each do |product|
-      puts "\t#{product.parent.title}"
-      puts "\t\t#{product.title}"
-    end
-    puts "Weighing #{(shopicruit.weight_of_variants(shopicruit.products_to_purchase)).to_f/1000} KGs"
-    puts "Costing you $#{shopicruit.price_of_variants(shopicruit.products_to_purchase)}"
-  end
-
 end
-
-Shopicruit.run
