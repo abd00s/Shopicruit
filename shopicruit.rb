0ed929b126f04fc0cf5265c3185b531818d62a02
Abdullah <abd00s@users.noreply.github.com>
2016-02-18 12:16:10 -0500

    Return run method to main class to avoid coupling

--- shopicruit.rb
+++ shopicruit.rb
@@ -90,4 +90,20 @@ class Shopicruit
     items.combination(i).select {|comb| weight_of_variants(comb) <= limit}
   end
 
+  def self.run
+    shopicruit = Shopicruit.new
+    shopicruit.filter_products
+    shopicruit.find_all_desirable_variants
+    shopicruit.find_carriable_combo(shopicruit.desirable_variants, shopicruit.limit)
+    puts shopicruit.message
+    shopicruit.products_to_purchase.each do |product|
+      puts "\t#{product.parent.title}"
+      puts "\t\t#{product.title}"
+    end
+    puts "Weighing #{(shopicruit.weight_of_variants(shopicruit.products_to_purchase)).to_f/1000} KGs"
+    puts "Costing you $#{shopicruit.price_of_variants(shopicruit.products_to_purchase)}"
+  end
+
 end
+
+Shopicruit.run
